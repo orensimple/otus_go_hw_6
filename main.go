84910b8cbca444737e587a5c6e23b22a81fa3db1
orensimple <orensimple@gmail.com>
2019-10-26 09:18:08 +0300

    fix comment after reviewer

--- main.go
+++ main.go
@@ -47,19 +47,22 @@ func copy(from string, to string, offset int64, limit int64) int {
 		fmt.Printf("offset more then file size")
 		return 0
 	}
-	println(fi.Size())
+	if limit == 0 {
+		limit = fi.Size()
+	}
+
 	if offset+limit > fi.Size() {
 		limit = fi.Size() - offset
 	}
 	bar := pb.StartNew(int(math.Round(float64(limit)/float64(bufSize) + 0.49)))
-	//fileTo, err := os.OpenFile(to, os.O_RDWR|os.O_CREATE, 0666)
+
 	fileTo, err := os.Create(to)
 	if err != nil {
 		fmt.Printf("Can't create/open file: %v %s", err, to)
 		return 0
 	}
 	var i int64
-	for i = 0; i < limit; i = i + bufSize {
+	for i = 0; i < limit; i += bufSize {
 		bar.Increment()
 		o3, err := fileFrom.Seek(int64(offset), 0)
 
@@ -72,7 +75,7 @@ func copy(from string, to string, offset int64, limit int64) int {
 		}
 		b3 := make([]byte, bufSize)
 		n3, err := io.ReadAtLeast(fileFrom, b3, int(bufSize))
-		count = count + n3
+		count += n3
 		if err == io.EOF {
 			bufSize = limit
 			continue
@@ -83,7 +86,7 @@ func copy(from string, to string, offset int64, limit int64) int {
 			fmt.Printf("failed to write: %v %d", err, a)
 			return count
 		}
-		offset = offset + bufSize
+		offset += bufSize
 	}
 	fileFrom.Close()
 	fileTo.Close()
